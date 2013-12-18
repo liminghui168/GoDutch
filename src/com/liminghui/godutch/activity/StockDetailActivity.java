9a79f31d039bfd856843394e1f5c9580918ac78f
liminghui168 <liminghuui168@qq.com>
2013-12-18 17:58:27 +0800

    get detail

--- src/com/liminghui/godutch/activity/StockDetailActivity.java
+++ src/com/liminghui/godutch/activity/StockDetailActivity.java
@@ -1,5 +1,11 @@
 package com.liminghui.godutch.activity;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.TabActivity;
 import android.content.Intent;
 import android.graphics.Color;
@@ -14,6 +20,9 @@ import android.widget.TabWidget;
 import android.widget.Toast;
 
 import com.liminghui.godutch.R;
+import com.liminghui.godutch.domain.Stock;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
 public class StockDetailActivity extends TabActivity {
 
@@ -35,6 +44,7 @@ public class StockDetailActivity extends TabActivity {
 	private String[] classArr; // Class数组
 	private String[] subClassArr; // SubClass数组
 	private int stockId;
+	private Stock mStock;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +55,8 @@ public class StockDetailActivity extends TabActivity {
 
 		Intent mIntent = this.getIntent();
 		stockId = mIntent.getIntExtra("stockId", 0);
+
+		getStockDetail();
 	}
 
 	private void initView() {
@@ -75,6 +87,8 @@ public class StockDetailActivity extends TabActivity {
 				"YESE" };
 		classArr = new String[] { "97", "AAAA", "BBBB", "cc" };
 
+		
+		
 		bt_stock_detail_find_vendor = (Button) findViewById(R.id.bt_stock_detail_find_vendor);
 
 		sp_stock_detail_unit = (Spinner) findViewById(R.id.sp_stock_detail_unit);
@@ -85,11 +99,11 @@ public class StockDetailActivity extends TabActivity {
 		sp_stock_detail_class = (Spinner) findViewById(R.id.sp_stock_detail_class);
 		sp_stock_detail_sub_class = (Spinner) findViewById(R.id.sp_stock_detail_sub_class);
 
+		mStock = new Stock();
 	}
 
 	private void bindData() {
 		setAdapter(sp_stock_detail_unit, unitArr);
-
 		setAdapter(sp_stock_detail_unit_price, unitPriceArr);
 		setAdapter(sp_stock_detail_ref_cost, refCostArr);
 		setAdapter(sp_stock_detail_brand, brandArr);
@@ -104,6 +118,61 @@ public class StockDetailActivity extends TabActivity {
 				android.R.layout.simple_spinner_item, items);
 		mAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		mSpinner.setAdapter(mAdapter);
+	}
+
+	private void getStockDetail() {
+		String path = getString(R.string.url_path) + "LoadStockDetail/"
+				+ stockId;
+		AsyncHttpClient client = new AsyncHttpClient();
+
+		client.get(path, new AsyncHttpResponseHandler() {
+
+			@Override
+			public void onSuccess(String content) {
+				if (content == "null") {
+					// 数据为空
+				} else {
+					parseJson(content);
+				}
+
+				super.onSuccess(content);
+			}
+
+		});
+	}
+
+	private void parseJson(String json) {
+		try {
+			JSONObject jsonObject = new JSONObject(json);
+			mStock.setId(jsonObject.getInt("Id"));
+			mStock.setStock(jsonObject.getString("stock1"));
+			mStock.setVendor(jsonObject.getString("vendor"));
+			mStock.setVendorname(jsonObject.getString("vendorname"));
+			mStock.setVref(jsonObject.getString("vref"));
+			mStock.setShortdes(jsonObject.getString("shortdes"));
+			mStock.setPower(jsonObject.getString("power"));
+			mStock.setOpowerrms(jsonObject.getString("opowerrms"));
+			mStock.setOpowerrms(jsonObject.getString("opowerpmpo"));
+			mStock.setPartno(jsonObject.getString("partno"));
+			mStock.setUnit(jsonObject.getString("unit"));
+			mStock.setPricecurr(jsonObject.getString("pricecurr"));
+			mStock.setPrice(jsonObject.getDouble("price"));
+			mStock.setCostcurr(jsonObject.getString("costcurr"));
+			mStock.setCost(jsonObject.getDouble("cost"));
+			mStock.setFirstmdate(jsonObject.getString("firstmdate"));
+			mStock.setPricecurr(jsonObject.getString("pricecurr"));
+			mStock.setBrand(jsonObject.getString("brand"));
+			mStock.setCategory(jsonObject.getString("category"));
+			mStock.setGbbarcode(jsonObject.getString("gbbarcode"));
+			mStock.setOcbarcode(jsonObject.getString("ocbarcode"));
+			mStock.setPbarcode(jsonObject.getString("pbarcode"));
+			mStock.setSclass(jsonObject.getString("sclass"));
+			mStock.setSubclass(jsonObject.getString("subclass"));
+			mStock.setAcvite(jsonObject.getBoolean("active"));
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
